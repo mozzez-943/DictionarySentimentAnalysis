a891802b34a1512f23bee62a4d407dfeb3762125
Moeez Omair <104922728+mozzez-943@users.noreply.github.com>
2024-11-10 08:07:01 -0500

    working code
    must euphemize words in the negative list

--- sentimentanalysis.py
+++ sentimentanalysis.py
@@ -26,3 +26,42 @@ def read_negative(file_path):
                 negative[category] = [attribute]
     return negative
 
+"""
+Reading the email.txt file and outputting the sentiment of the email.
+
+We want to convert the email into a list of words and then check if the words
+are in the positive or negative dictionary.
+
+If the word is in the positive dictionary, we increment the positive sentiment
+score by 1. If the word is in the negative dictionary, we increment the
+negative sentiment score by 1.
+
+We will also give an overall sentiment score based on the difference between
+the positive and negative sentiment scores.
+
+We will also reflect which key the words belong to in the positive and negative
+dictionary.
+"""
+
+def sentiment_analysis(email_path, positive, negative):
+    positive_score = 0
+    negative_score = 0
+    with open(email_path, mode='r') as file:
+        email = file.read().lower().split()
+        for word in email:
+            for key, value in positive.items():
+                if word in value:
+                    positive_score += 1
+                    print(f'+1: {word} is in the {key} category.')
+            for key, value in negative.items():
+                if word in value:
+                    negative_score += 1
+                    print(f'-1: {word} is in the {key} category.')
+    sentiment_score = positive_score - negative_score
+    print(f'Positive score: {positive_score}')
+    print(f'Negative score: {negative_score}')
+    print(f'Overall sentiment score: {sentiment_score}')
+
+positive = read_positive('positive.csv')
+negative = read_negative('negative.csv')
+sentiment_analysis('EASTEREGG.txt', positive, negative)
